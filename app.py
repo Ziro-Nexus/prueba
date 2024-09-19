c5a24112ab85e2cd98acb8e6963fef31d7868c1a
ZiroxDeveloper <ziroxdeveloper@outlook.com>
2024-09-19 08:30:22 -0500

    fix

--- app.py
+++ app.py
@@ -7,6 +7,5 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://testing:testing@mysql/flask_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# integrar instancia de base de datos 
 db.init_app(app)
 app.register_blueprint(inventory_service)
